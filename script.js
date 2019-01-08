d6d0900d4ef5f3208166089a2c8f6ee260b8339b
zariah2803 <43378752+zariah2803@users.noreply.github.com>
2019-01-08 09:52:45 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,25 @@
+//Spell #1
+
+$("#disaperia").click(function(){
+ $ ("#firstImg").fadeToggle();
+});
+
+
+
+
+//Spell #2
+
+$("      ").click(function(){
+ $("myclass2").hide();
+});
+
+
+
+//Spell #3
+
+$("      ").click(function(){
+ $("myclass4").slideup();
+});
+
+
+
